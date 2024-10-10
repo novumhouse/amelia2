1e70f1f3958b034542f865818745e207cef2c16f
Adrian Przybysz <35032840+PrzybyszAdrian@users.noreply.github.com>
2024-10-10 21:32:01 +0200

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <header className="text-white px-6 py-4 pb-0 flex items-center bg-[#0e0e0e]">
-      <span className="text-2xl font-bold">Voicetta</span>
+      <span className="text-2xl font-bold">Memory Walk</span>
     </header>
   );
 };
