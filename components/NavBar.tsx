2af1f12494ceed119d08c16d56106ffa743b1f30
Adrian Przybysz <35032840+PrzybyszAdrian@users.noreply.github.com>
2024-10-10 21:37:53 +0200

    Update NavBar.tsx

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -10,7 +10,7 @@ import {
   UserIcon,
 } from "lucide-react";
 import Link from "next/link";
-import React, { useState } from "react";
+import React from "react";
 import { usePathname } from "next/navigation";
 import LogInButton from "./login-btn";
 import { useSession } from "next-auth/react";
@@ -18,16 +18,14 @@ import { useSession } from "next-auth/react";
 const NavBar = () => {
   const pathname = usePathname();
   const session = useSession();
-  const [isSidebarVisible, setIsSidebarVisible] = useState(true);
-  
   // @ts-ignore
   const admin = session.data?.user?.admin;
 
   let navItems = [
     { href: "/dashboard", icon: ViewIcon, label: "Overview" },
     { href: "/assistants", icon: ActivityIcon, label: "Assistants" },
-    /* { href: "/call-logs", icon: PhoneCallIcon, label: "Call Logs" },
-    { href: "/phone-numbers", icon: PhoneIcon, label: "Phone Numbers" }, */
+   /* { href: "/call-logs", icon: PhoneCallIcon, label: "Call Logs" },
+    { href: "/phone-numbers", icon: PhoneIcon, label: "Phone Numbers" },*/
     { href: "/billing", icon: DollarSignIcon, label: "Billing" },
     { href: "/support", icon: HeadsetIcon, label: "Support" },
   ];
@@ -47,38 +45,31 @@ const NavBar = () => {
       <div>
         <header className="text-white px-6 py-4 pb-0 flex items-center bg-[#0e0e0e]">
           <Link href={"/"}>
-            <span className="text-2xl font-bold">Voicetta</span>
+            <span className="text-2xl font-bold">Memory Walk</span>
           </Link>
-          <button
-            onClick={() => setIsSidebarVisible(!isSidebarVisible)}
-            className="ml-auto p-2 text-white bg-[#183534] rounded"
-          >
-            {isSidebarVisible ? "Hide Menu" : "Show Menu"}
-          </button>
         </header>
-        {isSidebarVisible && (
-          <aside className="flex text-white w-64 py-8 px-4">
-            <ul className="space-y-4">
-              {navItems.map((item) => {
-                const isActive = pathname === item.href;
-                return (
-                  <Link key={item.href} href={item.href}>
-                    <li
-                      className={`p-2 my-4 rounded-lg font-bold ${
-                        isActive ? "bg-[#18282a] text-[#63beb7]" : ""
-                      }`}
-                    >
-                      <item.icon className="h-5 w-5 inline-block mr-2" />
-                      {item.label}
-                    </li>
-                  </Link>
-                );
-              })}
-            </ul>
-          </aside>
-        )}
+        <aside className="flex text-white w-64 py-8 px-4">
+          <ul className="space-y-4">
+            {navItems.map((item) => {
+              const isActive = pathname === item.href;
+              return (
+                <Link key={item.href} href={item.href}>
+                  <li
+                    className={p-2 my-4 rounded-lg font-bold ${
+                      isActive ? "bg-[#18282a] text-[#63beb7]" : ""
+                    }}
+                  >
+                    <item.icon className="h-5 w-5 inline-block mr-2" />
+                    {item.label}
+                  </li>
+                </Link>
+              );
+            })}
+          </ul>
+        </aside>
       </div>
       <div className="flex items-center m-4 ">
+        {/* <UserIcon className="h-8 w-8 mr-2" /> */}
         <LogInButton />
       </div>
     </div>
